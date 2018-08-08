fe2663d0e8a7c6dd292adbfb410a0892a976120b
karen S <buzz.kyt@gmail.com>
2018-08-08 03:16:22 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Boa
+def display_board
+  put [" ","|"," ","|"," ",]
+  put "-----------"
+  put [" ","|"," ","|"," ",]
+  put "-----------"
+  put [" ","|"," ","|"," ",]
+end
